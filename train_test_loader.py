13ad54288e0e8171689c686d22fdf43e597060d4
MamtaVenugopal <42015212+MamtaVenugopal@users.noreply.github.com>
2023-07-13 10:02:46 +0300

    Update train_test_loader.py

--- train_test_loader.py
+++ train_test_loader.py
@@ -7,29 +7,27 @@ def load(train_transform,test_transform,seed=1,batch_size=128,num_workers=4,pin_
 	
 
 	#Get the Train and Test Set
-	trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-	testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
-
-
-	SEED = 1
+  train = Cifar10SearchDataset(root='./data', train=True,download=True, transform=train_transforms)
+  test = Cifar10SearchDataset(root='./data', train=False,download=True, transform=test_transforms)
+  SEED = 1
 
 	# CUDA?
-	cuda = torch.cuda.is_available()
+  cuda = torch.cuda.is_available()
 
 
 	# For reproducibility
-	torch.manual_seed(SEED)
+  torch.manual_seed(SEED)
 
-	if cuda:
+  if cuda:
 			torch.cuda.manual_seed(SEED)
 
 	# dataloader arguments - something you'll fetch these from cmdprmt
-	dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory) if cuda else dict(shuffle=True, batch_size=64)
+  dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory) if cuda else dict(shuffle=True, batch_size=64)
 
-	trainloader = torch.utils.data.DataLoader(trainset, **dataloader_args)
-	testloader = torch.utils.data.DataLoader(testset, **dataloader_args)
+  trainloader = torch.utils.data.DataLoader(train, **dataloader_args)
+  testloader = torch.utils.data.DataLoader(test, **dataloader_args)
 
-	classes = ('plane', 'car', 'bird', 'cat',
+  classes = ('plane', 'car', 'bird', 'cat',
     	       'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-	return classes, trainloader, testloader
+  return classes, trainloader, testloader
