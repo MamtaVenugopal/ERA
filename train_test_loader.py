8bc2d589f7fa26eac063e1db7c822b48aa15f0ef
MamtaVenugopal <42015212+MamtaVenugopal@users.noreply.github.com>
2023-07-13 10:03:39 +0300

    Update train_test_loader.py

--- train_test_loader.py
+++ train_test_loader.py
@@ -1,7 +1,23 @@
 import torch
 import torchvision
 
+class Cifar10SearchDataset(datasets.CIFAR10):
 
+    def __init__(self, root="~/data", train=True, download=True, transform=None):
+
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+
+            transformed = self.transform(image=image)
+
+            image = transformed["image"]
+
+        return image, label
 
 def load(train_transform,test_transform,seed=1,batch_size=128,num_workers=4,pin_memory=True):
 	
